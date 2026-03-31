4a7154811973b13e7ed4a23f2bb8528512eb8bd7
satyamp11 <imsattu8601@gmail.com>
2026-03-31 20:33:21 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     <h1>Welcome to RentLoop 🚀</h1>
-     <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">Click me</button>
+   <Button variant="outline">Get Started</Button>
   
 
    </div>
